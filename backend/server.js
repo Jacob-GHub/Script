6253bb9107c25da8c28d43d74f3f43e16c3550b5
Jacob <110055706+frog187man@users.noreply.github.com>
2024-09-25 19:32:59 -0700

    not sure

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,29 @@
 import express from "express"
 import cors from "cors"
+require('dotenv').config();
 
 // app config
 const app = express()
 const port  = 4000
+
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database!');
+});
 
 //middleware
 app.use(express.json())
